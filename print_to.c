fd98da3bd9e48ee8d35864b9ba6432f68c4fd500
kabro_c <charbel.kabro@gmail.com>
2017-05-24 14:07:24 +0200

    first commit

--- print_to.c
+++ print_to.c
@@ -7,8 +7,8 @@
 #include "binary_tree.h"
 
 void print_to(t_tree *tree, int nb) {
-    while (tree) {
-        if (tree->nb >= nb) {
+    while (tree->nb != nb) {
+        if (tree->nb > nb) {
             printf("%d\n", tree->nb);
         }
         tree = tree->right;
