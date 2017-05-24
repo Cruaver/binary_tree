4340591739b6f85680e585e8d9e0dfcd06e2c4d5
kabro_c <charbel.kabro@gmail.com>
2017-05-24 11:47:53 +0200

    first commit

--- binary_tree.c
+++ binary_tree.c
@@ -6,12 +6,9 @@
 #include "binary_tree.h"
 
 void binary_tree(t_tree **tree, int nb) {
-
-    t_tree *tree;
-
     t_tree *node = malloc(sizeof(t_tree));
 
-    if (nb >= tree->nb) {
+    if (nb >= node->nb) {
         node->nb = nb;
         node->right = *tree;
     } else {
