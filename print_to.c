9c4460c70eb451e3b77f9e87b2c56b14f00b9716
kabro_c <charbel.kabro@gmail.com>
2017-05-24 11:24:50 +0200

    first commit

--- print_to.c
+++ print_to.c
@@ -2,10 +2,15 @@
  Created by Charbel KABRO on 24/05/17.
 */
 
-void print_to(tree, nb) {
+#include <stdlib.h>
+
+void print_to(t_tree *tree, nb) {
+
     while (tree) {
+
         printf("%d\n", tree->nb);
         tree = tree->right;
         tree = tree->left;
+
     }
 }
