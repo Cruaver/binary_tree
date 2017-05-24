0dcd2afa3e0f48382a387ba49f37a758deb8b375
kabro_c <charbel.kabro@gmail.com>
2017-05-24 14:56:29 +0200

    first commit

--- print_to.c
+++ print_to.c
@@ -7,11 +7,26 @@
 #include "binary_tree.h"
 
 void print_to(t_tree *tree, unsigned int nb) {
-    if (!tree)
+
+    /*
+        if (!tree)
         return;
     if (tree->left)
         print_to(tree->left, nb);
     printf("%d\n", tree->nb);
     if (tree->right)
         print_to(tree->right, nb);
+    */
+
+    while (tree) {
+        if (nb == tree->nb) {
+            printf("%d\n", tree->nb);
+            break;
+        }
+        if (nb > tree->nb)
+            tree = tree->right;
+        else
+            tree = tree->left;
+    }
+
 }
