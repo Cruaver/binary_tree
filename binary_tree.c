b9c7b73419be9e464eed5abf311ae8273f175629
kabro_c <charbel.kabro@gmail.com>
2017-05-24 17:56:37 +0200

    first commit

--- binary_tree.c
+++ binary_tree.c
@@ -18,23 +18,6 @@ int binary_tree(t_tree **trees, signed int nb) {
         node->nb = nb;
         node->right = NULL;
         node->left = NULL;
-        /*  if (tree)
-              do {
-                  tmpnode = tree;
-                  if (nb >= tree->nb) {
-                      tree = tree->right;
-                      if (!tree)
-                          tmpnode->right = node;
-                  } else {
-                      tree = tree->left;
-                      if (!tree)
-                          tmpnode->left = node;
-                  }
-              } while (tree);
-          else
-              *trees = node;
-
-      } */
         if (tree) {
             while (tree) {
                 tmpnode = tree;
