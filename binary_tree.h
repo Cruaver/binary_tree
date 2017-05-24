4b4d091b0a3c60cc70d317e2694f79e6d2f8c9ab
kabro_c <charbel.kabro@gmail.com>
2017-05-24 10:53:29 +0200

    first commit

--- binary_tree.h
+++ binary_tree.h
@@ -5,15 +5,11 @@ Created by Charbel KABRO on 24/05/17.
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
-struct s_tree {
-
+typedef struct s_tree {
     int nb;
-    t_tree *left;
-    t_tree *right;
-
+    struct s_tree *left;
+    struct s_tree *right;
 } t_tree;
-
-typedef struct t_tree t_tree;
 
 void binary_tree(t_tree &tree, int nb);
 void print_to(t_tree tree, int nb);
